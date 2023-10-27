4703266304a119e6ecb851b6ce2f6e12d5b29e2d
Sachin Gupta <sachin@hubilo.com>
2023-10-27 14:58:52 +0530

    set of icons which can we used later

---
+++ src/FileExplorer/icon.ts
@@ -0,0 +1,8 @@
+const icons = {
+  folder: "https://img.icons8.com/plasticine/100/folder-invoices.png",
+  file: "https://img.icons8.com/plasticine/100/file.png",
+  sortDown: "https://img.icons8.com/material-sharp/24/sort-down.png",
+  sortUp: "https://img.icons8.com/material-sharp/24/sort-right.png",
+};
+
+export default icons;
