51b44fcfc990905cb86e4bbde857504a54a39150
Sachin Gupta <sachin@hubilo.com>
2023-10-27 15:19:57 +0530

    Left Click | Selected will show on file with blue file name

--- src/FileExplorer/index.tsx
+++ src/FileExplorer/index.tsx
@@ -1,32 +1,53 @@
-
 import { useState } from "react";
 import "./styles.css"
 import CustomImg from "./CustomImg";
 import icons from "./icon";
 import { IParentData } from "./interface";
 
+const FileExplorer: React.FC<{ files: IParentData }> = ({ files }) => {
+  const [isExpanded, toggleExpanded] = useState(false); // to track folder expansion
+  const [isSelected, toggleSelected] = useState(false); // to track selected state  | here when its selected it will be blue in color
 
-const FileExplorer: React.FC<{ files:IParentData}> = ({ files }) => {
-    const [isExpanded, toggleExpanded] = useState(false); // if will applicable on toggle icon click
-    const isFilesArr = Boolean(Array.isArray(files.data) && files.data.length); // need to make in folders we have data or not
+  const isFilesArr = Boolean(Array.isArray(files.data) && files.data.length);
 
+  const handleFolderClick = () => {
+    toggleExpanded(!isExpanded);
+  };
 
-    if (files.type === "file") {
-        return (<div className="file-name"> <CustomImg url={files.imgUrl || ''} /> {files.name} </div>);
-    }
+  const handleFileClick = () => {
+    toggleSelected(!isSelected);
+  };
 
+  if (files.type === "file") {
     return (
-        <div className="folder">
-            <div className="folder-title" onClick={() => toggleExpanded(!isExpanded)}>
-                <CustomImg url={files.imgUrl || ''} /> {files.name} <CustomImg url={isExpanded ? icons.sortDown :icons.sortUp} height="10px" width="10px" />
-            </div>
-            {isExpanded && isFilesArr ? (
-                files.data.map((item) => <FileExplorer files={item as IParentData} />)
-            ) : (
-                <></>
-            )}
-        </div>
+      <div
+        className={`file-name ${isSelected ? 'selected' : ''}`}
+        onClick={handleFileClick}
+      >
+        <CustomImg url={files.imgUrl || ''} /> {files.name}
+      </div>
     );
+  }
+
+  return (
+    <div className="folder">
+      <div className={`folder-title ${isSelected ? 'selected' : ''}`} onClick={handleFolderClick}>
+        <CustomImg url={files.imgUrl || ''} /> {files.name}
+        <CustomImg
+          url={isExpanded ? icons.sortDown : icons.sortUp}
+          height="10px"
+          width="10px"
+        />
+      </div>
+      {isExpanded && isFilesArr ? (
+        files.data.map((item, index) => (
+          <FileExplorer key={index} files={item as IParentData} />
+        ))
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 
 export default FileExplorer;
